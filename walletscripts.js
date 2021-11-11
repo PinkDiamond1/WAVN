4077ba9654cbb19759724f579f45194106c0ba42
JunaidPeer <87606844+JunaidPeer@users.noreply.github.com>
2021-11-11 09:46:19 +0530

    WAVN

--- walletscripts.js
+++ walletscripts.js
@@ -1,6 +1,6 @@
 async function addToMetamask() {
 	if (typeof window.ethereum !== "undefined") {
 		await ethereum.request({ method: 'wallet_addEthereumChain', params: [{chainId: '0x89',chainName: 'Polygon',nativeCurrency:{name: 'Matic',symbol: 'Matic',decimals: 18},rpcUrls: ['https://rpc-mainnet.maticvigil.com/'],blockExplorerUrls :['https://polygonscan.com/'],}]})
-		await await ethereum.request({method: 'wallet_watchAsset',params: {type: 'ERC20',options: {address: "0x56F483CF2f1F6cf224656647CA0a0D11BFB0404E",symbol: "WRVL",decimals: 18,image: "https://i1.wp.com/ravencoinlite.info/wp-content/uploads/2021/09/RVL-transparent-bg.png?w=1080&ssl=1",},},});
+		await await ethereum.request({method: 'wallet_watchAsset',params: {type: 'ERC20',options: {address: "0x752dc265eaf6da2db0f8e4a32d5596d3f18e8701",symbol: "WAVN",decimals: 18,image: "https://github.com/AvianNetwork/WAVN/blob/master/avn512x512x32.png?raw=true",},},});
 	}
 }
